8a9a8442d21b45341d2d239ab44367d087c267e5
duyducpm0120 <ducduypm0120@gmail.com>
2021-10-11 21:01:31 +0700

    Import Icons

---
+++ SwipeBike/src/constants/icons.js
@@ -0,0 +1,6 @@
+export const cameraBold = require('../assets/icons/Bold/camera-bold.png');
+export const email = require('../assets/icons/Light/email.png');
+export default {
+  cameraBold,
+  email,
+};
