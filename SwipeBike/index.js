910d7eeab2f313fff3e578c8e6c368d1f7ded683
duyducpm0120 <ducduypm0120@gmail.com>
2021-10-17 19:31:30 +0700

    Temporary UpdateProfile commit

--- SwipeBike/index.js
+++ SwipeBike/index.js
@@ -6,11 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import {Provider} from 'react-redux';
+import {Provider as PaperProvider} from 'react-native-paper';
 import store from './src/redux/store';
 function app() {
   return (
     <Provider store={store}>
-      <App></App>
+      <PaperProvider>
+        <App></App>
+      </PaperProvider>
     </Provider>
   );
 }
--- SwipeBike/src/screens/updateProfile.js
+++ SwipeBike/src/screens/updateProfile.js
@@ -3,9 +3,16 @@ import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {FONTS, SIZES, COLORS, PIXEL, ICONS, IMAGES, STYLES} from '../constants';
 import {BackgroundButton, RoundedImage} from '../components';
 import DatePicker from 'react-native-date-picker';
-import {TextInput, configureFonts, Provider, List} from 'react-native-paper';
+import {
+  TextInput,
+  configureFonts,
+  Provider,
+  List,
+  RadioButton,
+} from 'react-native-paper';
 import DropDown from 'react-native-paper-dropdown';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const fontConfig = {
   default: {
@@ -21,28 +28,8 @@ export default function UpdateProfile(props) {
   const [name, setName] = useState();
   const [sex, setSex] = useState('');
 
-  //Dropdown field
-  const [showDropDown, setShowDropDown] = useState(false);
-
-  const genderList = [
-    {
-      label: 'Nam',
-      value: 'male',
-    },
-    {
-      label: 'Nữ',
-      value: 'female',
-    },
-    {
-      label: 'Khác',
-      value: 'others',
-    },
-  ];
-
-  //Expander field
-  const [expanded, setExpanded] = React.useState(true);
-
-  const handlePress = () => setExpanded(!expanded);
+  //Sex radioButton field
+  const [checked, setChecked] = React.useState('first');
 
   //DatePicker Field
   const [date, setDate] = useState(new Date());
@@ -54,7 +41,7 @@ export default function UpdateProfile(props) {
         style={{
           justifyContent: 'center',
           alignItems: 'center',
-          marginBottom: 20,
+          marginBottom: 10,
         }}>
         <Text style={FONTS.title}>Cập nhật hồ sơ</Text>
         <Text style={FONTS.h3}>
@@ -100,24 +87,26 @@ export default function UpdateProfile(props) {
   }
   function renderTextField() {
     return (
-      <View
-        style={{
+      <ScrollView
+        contentContainerStyle={{
           justifyContent: 'center',
           alignItems: 'center',
           flexDirection: 'column',
           width: PIXEL.pixelSizeHorizontal(330),
-        }}>
+        }}
+        showsVerticalScrollIndicator={false}>
+        {/* Name */}
         <View
           style={{
-            flexDirection: 'row',
+            flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'flex-start',
             width: '100%',
             marginVertical: PIXEL.pixelSizeVertical(10),
           }}>
+          <Text style={{...FONTS.h2Bold}}>Tên đại diện</Text>
           <TextInput
-            label="Tên đại diện"
-            mode="outlined"
+            //mode="outlined"
             value={name}
             theme={{
               colors: {
@@ -127,77 +116,109 @@ export default function UpdateProfile(props) {
             style={{
               width: '100%',
               height: PIXEL.pixelSizeVertical(50),
-              fontSize: PIXEL.fontPixel(18),
+              fontSize: SIZES.h3,
               paddingHorizontal: 0,
+              backgroundColor: COLORS.backGroundColor,
             }}
             onChangeText={name => setName(name)}></TextInput>
         </View>
 
+        {/* Sex */}
         <View
           style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-start',
-            alignItems: 'center',
-            borderRadius: 10,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
             width: '100%',
-            marginVertical: PIXEL.pixelSizeVertical(10),
+            marginBottom: PIXEL.pixelSizeVertical(10),
           }}>
-          {/* Dropdown field */}
-          <List.Section title="Accordions" style={{width: '100%'}}>
-            <List.Accordion
-              title="Uncontrolled Accordion"
-              left={props => <List.Icon {...props} icon="folder" />}>
-              <List.Item title="First item" />
-              <List.Item title="Second item" />
-            </List.Accordion>
-
-            <List.Accordion
-              title="Controlled Accordion"
-              left={props => <List.Icon {...props} icon="folder" />}
-              expanded={expanded}
-              onPress={handlePress}>
-              <List.Item title="First item" />
-              <List.Item title="Second item" />
-            </List.Accordion>
-          </List.Section>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'flex-start',
-            alignItems: 'center',
-            borderRadius: 10,
-            backgroundColor: COLORS.primaryLighter1,
-            width: '100%',
-            height: PIXEL.pixelSizeVertical(50),
-            marginVertical: PIXEL.pixelSizeVertical(10),
-            paddingHorizontal: PIXEL.pixelSizeHorizontal(5),
-          }}>
-          <Image
-            source={ICONS.birthday}
+          <Text style={{...FONTS.h2Bold}}>Giới tính</Text>
+          <RadioButton.Group
+            onValueChange={value => setSex(value)}
+            value={sex}
             style={{
-              width: PIXEL.pixelSizeHorizontal(24),
-              height: PIXEL.pixelSizeHorizontal(24),
-              tintColor: COLORS.lightGray1,
-            }}></Image>
-          <Text
-            style={{
-              ...FONTS.h3,
-              marginLeft: PIXEL.pixelSizeHorizontal(15),
-              width: PIXEL.pixelSizeHorizontal(240),
-              paddingLeft: 5,
+              width: '100%',
+              paddingHorizontal: 0,
             }}>
-            {date.toDateString()}
-          </Text>
-          <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
-            <Image
-              source={ICONS.edit}
+            <RadioButton.Item
+              label="Nam"
+              labelStyle={{
+                fontSize: SIZES.h3,
+              }}
+              value="male"
+              color={COLORS.primary}
+              uncheckedColor={COLORS.darkgray}
               style={{
-                width: PIXEL.pixelSizeHorizontal(24),
-                height: PIXEL.pixelSizeHorizontal(24),
-                tintColor: COLORS.lightGray1,
-              }}></Image>
-          </TouchableOpacity>
+                width: '75%',
+                fontSize: SIZES.h3,
+                paddingHorizontal: 0,
+                marginHorizontal: 0,
+              }}
+            />
+            <RadioButton.Item
+              label="Nữ"
+              labelStyle={{fontSize: SIZES.h3}}
+              value="female"
+              color={COLORS.primary}
+              uncheckedColor={COLORS.darkgray}
+              style={{
+                width: '75%',
+                fontSize: SIZES.h2,
+                paddingHorizontal: 0,
+                marginHorizontal: 0,
+              }}
+            />
+            <RadioButton.Item
+              label="Khác"
+              labelStyle={{fontSize: SIZES.h3}}
+              value="others"
+              color={COLORS.primary}
+              uncheckedColor={COLORS.darkgray}
+              style={{
+                width: '75%',
+                fontSize: SIZES.h3,
+                paddingHorizontal: 0,
+                marginHorizontal: 0,
+              }}
+            />
+          </RadioButton.Group>
+        </View>
+
+        {/* birthDay */}
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            width: '100%',
+            marginBottom: PIXEL.pixelSizeVertical(10),
+          }}>
+          <Text style={{...FONTS.h2Bold}}>Sinh nhật</Text>
+          <View
+            style={{
+              width: '100%',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              flexDirection: 'row',
+            }}>
+            <Text
+              style={{
+                ...FONTS.h3,
+                width: PIXEL.pixelSizeHorizontal(240),
+              }}>
+              {date.toDateString()}
+            </Text>
+            <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
+              <Image
+                source={ICONS.edit}
+                style={{
+                  width: PIXEL.pixelSizeHorizontal(24),
+                  height: PIXEL.pixelSizeHorizontal(24),
+                  tintColor: COLORS.lightGray1,
+                }}></Image>
+            </TouchableOpacity>
+          </View>
+
           <DatePicker
             modal
             mode={'date'}
@@ -212,25 +233,52 @@ export default function UpdateProfile(props) {
             }}
           />
         </View>
-      </View>
+
+        {/* University */}
+        <View
+          style={{
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'flex-start',
+            width: '100%',
+            marginVertical: PIXEL.pixelSizeVertical(10),
+          }}>
+          <Text style={{...FONTS.h2Bold}}>Tên đại diện</Text>
+          <TextInput
+            //mode="outlined"
+            value={name}
+            theme={{
+              colors: {
+                primary: COLORS.primary,
+              },
+            }}
+            style={{
+              width: '100%',
+              height: PIXEL.pixelSizeVertical(50),
+              fontSize: SIZES.h3,
+              paddingHorizontal: 0,
+              backgroundColor: COLORS.backGroundColor,
+              marginVertical: 0,
+            }}
+            onChangeText={name => setName(name)}></TextInput>
+        </View>
+      </ScrollView>
     );
   }
 
   return (
-    <Provider>
-      <SafeAreaView style={STYLES.container}>
-        {renderHeader()}
-        {renderImagePicker()}
-        {renderTextField()}
-        <TouchableOpacity
-          style={{
-            position: 'absolute',
-            marginTop: PIXEL.pixelSizeVertical(615),
-          }}
-          onPress={() => {}}>
-          <BackgroundButton text="Xong"></BackgroundButton>
-        </TouchableOpacity>
-      </SafeAreaView>
-    </Provider>
+    <SafeAreaView style={STYLES.container}>
+      {renderHeader()}
+      {renderImagePicker()}
+      {renderTextField()}
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          marginTop: PIXEL.pixelSizeVertical(615),
+        }}
+        onPress={() => {}}>
+        <BackgroundButton text="Xong"></BackgroundButton>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
