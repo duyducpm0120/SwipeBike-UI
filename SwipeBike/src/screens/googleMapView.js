2532c9e5d68621261a7c8f5c39c675bf9d74c8be
duyducpm0120 <ducduypm0120@gmail.com>
2021-11-02 14:54:01 +0700

    Install geolocation service and add getCurrentLocation func

--- SwipeBike/src/screens/googleMapView.js
+++ SwipeBike/src/screens/googleMapView.js
@@ -1,6 +1,96 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Tex, Alert, PermissionsAndroid} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import Geolocation from 'react-native-geolocation-service';
+
+export default function GoogleMapView() {
+  const [locationPermission, setLocationPermission] = useState();
+
+  //User current location
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 14.1693,
+    longitude: 109.0495,
+    coordinates: [14.1693, 109.0495],
+  });
+
+  function requestLocationPermission() {
+    if (locationPermission === PermissionsAndroid.RESULTS.GRANTED) {
+      () => {
+        console.log(locationPermission);
+        return;
+      };
+    }
+    try {
+      const granted = PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'SwipeBike App',
+          message: 'Permit SwipeBike access to your location ',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        //console.log('You can use the location');
+        //alert('You can use the location');
+        setLocationPermission(granted);
+      } else {
+        //console.log('location permission denied');
+        //alert('Location permission denied');
+      }
+    } catch (err) {
+      //console.warn(err);
+    }
+  }
+
+  async function getCurrentLocation() {
+    if (locationPermission !== PermissionsAndroid.RESULTS.GRANTED)
+      await requestLocationPermission();
+    await Geolocation.getCurrentPosition(
+      position => {
+        if (
+          position.coords.latitude === currentLocation.latitude &&
+          position.coords.longitude === currentLocation.longitude
+        )
+          return;
+        setCurrentLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: currentLocation.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+      error => {
+        //Alert.alert(error.message.toString());
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+      },
+    );
+    console.log(currentLocation);
+  }
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, [currentLocation]);
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
@@ -13,19 +103,3 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
   },
 });
-
-export default function GoogleMapView() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-        style={styles.map}
-        region={{
-          latitude: 14.1693,
-          longitude: 109.0495,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}></MapView>
-    </View>
-  );
-}
