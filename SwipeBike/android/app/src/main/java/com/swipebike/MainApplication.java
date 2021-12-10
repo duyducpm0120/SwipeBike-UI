184a15cda54b4f1080288f81edd98c11ac70aaa6
Phan Duy Đức <63651728+duyducpm0120@users.noreply.github.com>
2021-12-10 10:25:56 +0700

    Can run android on MacOS

--- SwipeBike/android/app/src/main/java/com/swipebike/MainApplication.java
+++ SwipeBike/android/app/src/main/java/com/swipebike/MainApplication.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.facebook.react.bridge.JSIModulePackage; // <- add
+import com.swmansion.reanimated.ReanimatedJSIModulePackage; // <- add
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -34,6 +37,12 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage(); // <- add
+        }
+  
       };
 
   @Override
